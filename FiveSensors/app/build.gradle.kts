2981230568c0eb81ea45faeba57acef4d62405f7
Milana <milana.doborjginidze@student.nhlstenden.com>
2026-05-13 20:49:25 +0200

    fix: add lifecycle-viewmodel-compose dependency

--- FiveSensors/app/build.gradle.kts
+++ FiveSensors/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
