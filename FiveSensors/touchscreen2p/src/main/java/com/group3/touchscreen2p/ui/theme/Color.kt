b22ae2aa05f77dd997722708e44304d78677a800
AiNguyen <nguyenthoai200399@gmail.com>
2026-05-20 13:40:05 +0200

    Add color and theme

--- FiveSensors/touchscreen2p/src/main/java/com/group3/touchscreen2p/ui/theme/Color.kt
+++ FiveSensors/touchscreen2p/src/main/java/com/group3/touchscreen2p/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.group3.touchscreen2p.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Background
+val NavyBackground = Color(0xFF0A0E21)
+val NavySurface    = Color(0xFF111827)
+val NavyCard       = Color(0xFF1A2235)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Brand
+val YellowAccent   = Color(0xFFFFD60A)
+val WhitePrimary   = Color(0xFFFFFFFF)
+
+// Players
+val OrangePlayer1  = Color(0xFFFF6B35)
+val BluePlayer2    = Color(0xFF4A90D9)
+
+// Danger
+val RedDanger      = Color(0xFFFF3B30)
+
+// Target rings (outer to inner)
+val TargetRingOuter = Color(0xFF1E3A5F)
+val TargetRingMid   = Color(0xFF2E6DB4)
+val TargetRingInner = Color(0xFFFFFFFF)
+
+// Overlay
+val DimOverlay = Color(0xB3000000)
